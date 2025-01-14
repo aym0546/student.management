5da4ba2b789143117a89c52aeff97894ea6f3824
aym0546 <ciany.f@gmail.com>
2025-01-14 20:05:51 +0900

    MyBatisによるDB取得・操作機能を追加

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,40 +1,57 @@
 package raisetech.student.management;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-//	生徒情報のマップstudent<名前,年齢>作成
-	private Map<String, String> student = new HashMap<>();
+//	インターフェースStudentRepositoryを呼び出し
+	@Autowired
+	private StudentRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	// 情報取得メソッド（全員）
-	@GetMapping("/studentInfo")
-	public Map<String, String> getStudentInfo() {
-		return student;
+//	情報取得メソッド
+	@GetMapping("/student")
+	public String getStudent(@RequestParam String name) {
+		Student student = repository.searchByName(name);
+		return student.getName() + " " + student.getAge() + "歳";
 	}
 
-//	生徒情報のセット
-	@PostMapping("/studentInfo")
-	public void setStudentInfo(String name, String age) {
-		student.put(name, age);
+//	生徒情報の一覧表示
+	@GetMapping("/students")
+	public List<Student> listDisplay() {
+		return repository.display();
 	}
 
-//	生徒の名前をキーに年齢を表示
-	@PostMapping("/studentAge")
-	public String getStudentAge(String name) {
-		return student.get(name) + "歳";
+//	生徒情報の登録
+	@PostMapping("/student")
+	public void registerStudent(String name, int age) {
+		repository.registerStudent(name, age);
+	}
+
+//	生徒情報の変更
+	@PatchMapping("/student")
+	public void updateStudent(String name, int age) {
+		repository.updateStudent(name, age);
+	}
+
+//	生徒情報の削除
+	@DeleteMapping("/student")
+	public void deleteStudent(String name) {
+		repository.deleteStudent(name);
 	}
 
 }
